eaf32908dc52ad2c4cf2f1deaf662a12636b5271
Alora Riley <alora.riley@gmail.com>
2024-10-23 17:44:01 -0400

    feat: Add if else conditions for sorting to the index action

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::PostersController < ApplicationController
   def index
     posters = Poster.all
-    render json: PosterSerializer.format_posters(posters)
+    if params[:sort] == "asc"
+    sorted_asc = posters.sort_by(&:created_at)
+      render json: PosterSerializer.format_posters(sorted_asc)
+    elsif params[:sort] == "desc"
+      sorted_desc = posters.sort_by(&:created_at).reverse
+      render json: PosterSerializer.format_posters(sorted_desc)
+    else  
+      render json: PosterSerializer.format_posters(posters)
+    end
   end
 
   def show
